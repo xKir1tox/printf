b41d803c67c1e8c1d94080d6bef5f42152843623
Wanyika2 <dorothywanyika4@gmail.com>
2023-09-18 11:36:38 -0700

    I am not going anywhere

--- _printf.c
+++ _printf.c
@@ -23,7 +23,7 @@ int _printf(const char *format, ...)
 	{
 	if (format[i] != '%')
 	{
-	buffer[buff_ind++] = format[i]
+	buffer[buff_ind++] = format[i];
 	if (buff_ind == BUFF_SIZE)
 	print_buffer(buffer, &buff_ind);
 	/* write(1, &format[i], 1);*/
